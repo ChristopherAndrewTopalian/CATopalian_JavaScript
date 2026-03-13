4638541c99daec2bfcb0314a53ddd87d2315587a
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-03-13 17:55:39 -0400

    update

--- src/js/3data/data_001.js
+++ src/js/3data/data_001.js
@@ -78,6 +78,12 @@ let operators = [
     },
 
     {
+        name: "concat()",
+        urlOffline: "src/html/array/concat.html",
+        category: 'array'
+    },
+
+    {
         name: "Array of Objects",
         urlOffline: "src/html/array_of_objects/array_of_objects.html",
         category: 'array_of_objects'
