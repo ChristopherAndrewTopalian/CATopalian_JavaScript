8f3972c0416e47b7d21687b32d31d3b673bf4763
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-03-13 16:57:47 -0400

    update

--- src/js/3data/data_001.js
+++ src/js/3data/data_001.js
@@ -56,6 +56,12 @@ let operators = [
     },
 
     {
+        name: "includes()",
+        urlOffline: "src/html/array/includes.html",
+        category: 'array'
+    },
+
+    {
         name: "Array of Objects",
         urlOffline: "src/html/array_of_objects/array_of_objects.html",
         category: 'array_of_objects'
