2c3c8eab8bd81ce095b406f33296391bd3a381e8
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-03-13 16:54:49 -0400

    update

--- src/js/3data/data_001.js
+++ src/js/3data/data_001.js
@@ -50,6 +50,12 @@ let operators = [
     },
 
     {
+        name: "slice()",
+        urlOffline: "src/html/array/slice.html",
+        category: 'array'
+    },
+
+    {
         name: "Array of Objects",
         urlOffline: "src/html/array_of_objects/array_of_objects.html",
         category: 'array_of_objects'
