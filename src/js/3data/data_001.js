efd5f0873bf211144e98ddca389d8fe0977a42b1
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-03-13 18:02:04 -0400

    update

--- src/js/3data/data_001.js
+++ src/js/3data/data_001.js
@@ -84,6 +84,12 @@ let operators = [
     },
 
     {
+        name: "copyWithin()",
+        urlOffline: "src/html/array/copyWithin.html",
+        category: 'array'
+    },
+
+    {
         name: "Array of Objects",
         urlOffline: "src/html/array_of_objects/array_of_objects.html",
         category: 'array_of_objects'
