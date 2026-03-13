13a9f4a53fd545aef0d4218bb143a1419d9bb006
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-03-13 17:00:19 -0400

    update

--- src/js/3data/data_001.js
+++ src/js/3data/data_001.js
@@ -62,6 +62,12 @@ let operators = [
     },
 
     {
+        name: "join()",
+        urlOffline: "src/html/array/join.html",
+        category: 'array'
+    },
+
+    {
         name: "Array of Objects",
         urlOffline: "src/html/array_of_objects/array_of_objects.html",
         category: 'array_of_objects'
