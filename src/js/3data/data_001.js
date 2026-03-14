defe2ff46f84a8292a9a3257ff589b965b723e03
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-03-14 04:58:53 -0400

    update

--- src/js/3data/data_001.js
+++ src/js/3data/data_001.js
@@ -180,6 +180,12 @@ let operators = [
     },
 
     {
+        name: "lastIndexOf()",
+        urlOffline: "src/html/array/lastIndexOf.html",
+        category: 'array'
+    },
+
+    {
         name: "Array of Objects",
         urlOffline: "src/html/array_of_objects/array_of_objects.html",
         category: 'array_of_objects'
