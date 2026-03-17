22505606fa167e53c7aed6189abdc7a81723d0ab
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-03-17 07:11:15 -0400

    update

--- src/js/3data/data_001.js
+++ src/js/3data/data_001.js
@@ -312,6 +312,12 @@ let operators = [
     },
 
     {
+        name: "every()",
+        urlOffline: "src/html/array_of_objects/every.html",
+        category: 'array_of_objects'
+    },
+
+    {
         name: "filter()",
         urlOffline: "src/html/array_of_objects/filter.html",
         category: 'array_of_objects'
