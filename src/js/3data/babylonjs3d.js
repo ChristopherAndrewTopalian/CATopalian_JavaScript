bc39900b4eccf990ab8b8f8fa5a2c7cf8df0cf29
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-03-21 07:57:51 -0400

    update

--- src/js/3data/babylonjs3d.js
+++ src/js/3data/babylonjs3d.js
@@ -24,7 +24,7 @@ let babylonjs3d =
 
     {
         name: 'Textures and Security',
-        appURL: 'src/html/babylonjs/textures/textures_and_security.html'
+        appURL: 'src/html/babylonjs/textures_and_security/textures_and_security.html'
     },
     
 ];
