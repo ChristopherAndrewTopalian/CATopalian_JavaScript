399df574bf264fa08f48104e3cdff59f0a5bf8b8
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-04-03 22:41:50 -0400

    update

--- src/js/3data/data_001.js
+++ src/js/3data/data_001.js
@@ -1110,6 +1110,12 @@ let operators = [
     },
 
     {
+        name: 'Contradiction',
+        urlOffline: 'src/html/true_ai/logic_gates/contradiction.html',
+        category: 'true_ai'
+    },
+
+    {
         name: 'Life Creation Engine Class Mammal',
         urlOffline: 'src/html/Life_Creation_Engine/Life_Creation_Engine_Class_Mammal/Life_Creation_Engine_Class_Mammal.html',
         category: 'life_creation_engine',
