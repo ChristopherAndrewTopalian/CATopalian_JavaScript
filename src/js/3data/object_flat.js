e42f527520dd89f3993a789046359dd636cea87c
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-07-08 12:05:33 -0400

    update

--- src/js/3data/object_flat.js
+++ src/js/3data/object_flat.js
@@ -11,7 +11,13 @@ let object_flat = [
         name: 'Object Flat 002',
         category: 'object',
         urlOffline: 'src/html/object_flat/object_flat_002.html'
-    }
+    },
+
+    {
+        name: 'Object Flat 003',
+        category: 'object',
+        urlOffline: 'src/html/object_flat/object_flat_003.html'
+    },
 ];
 
 //----//
