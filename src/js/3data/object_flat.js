0b99d2fa3989067743870357290b12521fc26c43
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-07-08 11:59:17 -0400

    update

--- src/js/3data/object_flat.js
+++ src/js/3data/object_flat.js
@@ -13,3 +13,12 @@ let object_flat = [
         urlOffline: 'src/html/object_flat/object_flat_002.html'
     }
 ];
+
+//----//
+
+// Dedicated to God the Father
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2026
+// https://github.com/ChristopherTopalian
+// https://github.com/ChristopherAndrewTopalian
+// https://sites.google.com/view/CollegeOfScripting
+
